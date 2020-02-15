903623f5fbc7538946e492b63e242ffa68a3e5a0
Stanislav Kinzl <kinzlstanislav@gymnachod.cz>
2020-02-15 17:52:46 +0100

    AccountsMapperTest

--- CSASUcty/lib_architecture_repository/src/test/java/com/kinzlstanislav/csasucty/architecture/repository/mapper/AccountsMapperTest.kt
+++ CSASUcty/lib_architecture_repository/src/test/java/com/kinzlstanislav/csasucty/architecture/repository/mapper/AccountsMapperTest.kt
@@ -1,5 +1,65 @@
 package com.kinzlstanislav.csasucty.architecture.repository.mapper
 
-import org.junit.Assert.*
+import com.kinzlstanislav.csasucty.architecture.network.response.AccountResponse
+import com.kinzlstanislav.csasucty.architecture.network.response.AccountsResponse
+import com.kinzlstanislav.csasucty.architecture.repository.model.Account
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.Test
 
-class AccountsMapperTest
+class AccountsMapperTest {
+
+    private companion object {
+        val INPUT = AccountsResponse(
+            listOf(
+                AccountResponse(
+                    accountNumber = "123",
+                    bankCode = "321",
+                    transparencyFrom = "2015-01-02T00:00:00",
+                    transparencyTo = "?",
+                    publicationTo = "?",
+                    actualizationDate = "?",
+                    balance = 1.0f,
+                    currency = "CZK",
+                    name = "Spolecenstvi Praha",
+                    iban = "12345"
+                ),
+                AccountResponse()
+            )
+        )
+        val EXPECTED_OUTPUT = listOf(
+            Account(
+                accountNumber = "123",
+                bankCode = "321",
+                transparencyFrom = "2015 01 02",
+                transparencyTo = "?",
+                publicationTo = "?",
+                actualizationDate = "?",
+                balance = 1.0f,
+                currency = "CZK",
+                name = "Spolecenstvi Praha",
+                iban = "12345"
+            ),
+            Account(
+                "",
+                "",
+                "",
+                "",
+                "",
+                "",
+                null,
+                "",
+                "",
+                ""
+            )
+        )
+    }
+
+    private val subject = AccountsMapper()
+
+    @Test
+    fun `mapAccounts()`() {
+        assertThat(subject.mapAccounts(INPUT))
+            .isEqualTo(EXPECTED_OUTPUT)
+    }
+
+}
